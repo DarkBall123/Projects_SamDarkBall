1e8f0e6d67e1ed8e3065650229317c967f924288
DarkBall123 <vitalya228007@gmail.com>
2026-03-21 17:41:45 +0300

    Fix Sting local O2Script execution

--- sting/run_create_drone_inter_lods.bat
+++ sting/run_create_drone_inter_lods.bat
@@ -1,8 +1,18 @@
 @echo off
 setlocal
 
+set "SCRIPT_DIR=%~dp0"
+
 if "%~1"=="" (
-	echo Usage: %~nx0 P:\sting\drone_inter.p3d
+	set "INPUT_P3D=%SCRIPT_DIR%drone_inter.p3d"
+) else (
+	set "INPUT_P3D=%~f1"
+)
+
+if not exist "%INPUT_P3D%" (
+	echo Input P3D not found: %INPUT_P3D%
+	echo Put drone_inter.p3d next to %~nx0 or pass a path explicitly.
+	echo Example: %~nx0 drone_inter.p3d
 	exit /b 1
 )
 
@@ -15,6 +25,6 @@ if not exist "%OB%\O2Script.exe" (
 )
 
 cd /d "%OB%"
-O2Script.exe -a "%~dp0create_drone_inter_lods.bio2s" "%~1"
+O2Script.exe -a "%SCRIPT_DIR%create_drone_inter_lods.bio2s" "%INPUT_P3D%"
 
 endlocal
