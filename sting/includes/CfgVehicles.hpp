a032b6ab67055d46ac2dfb58e62d89b25e898551
DarkBall123 <vitalya228007@gmail.com>
2026-03-22 15:58:35 +0300

    Increase Sting speed and battery alignment

--- sting/includes/CfgVehicles.hpp
+++ sting/includes/CfgVehicles.hpp
@@ -163,7 +163,7 @@ class CfgVehicles
 		{
 		};
 		startDuration = 3;
-		maxSpeed = 420;
+		maxSpeed = 520;
 		precision = 4;
 		// AI steering look-ahead and turn prediction distances in meters.
 		steerAheadSimul = 1.2;
@@ -172,8 +172,8 @@ class CfgVehicles
 		predictTurnSimul = 3.2;
 		liftForceCoef = 3.1;
 		cyclicAsideForceCoef = 0.45;
-		cyclicForwardForceCoef = 1.45;
-		bodyFrictionCoef = 0.32;
+		cyclicForwardForceCoef = 1.85;
+		bodyFrictionCoef = 0.24;
 		backRotorForceCoef = 4.8;
 		fuelCapacity = 3.0;
 		maxFordingDepth = 0.3;
